ad62676422560b6bbb31ecd49bb60b0f5cf24e42
Shital Karande <shitalrk21@gmail.com>
2024-08-28 21:50:25 +0530

    done2

---
+++ DestinationHashGenerator/src/main/java/com/example/destination/DestinationHashGenerator/DestinationHashGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.destination.DestinationHashGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinationHashGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinationHashGeneratorApplication.class, args);
+	}
+
+}
---
+++ DestinationHashGenerator/src/test/java/com/example/destination/DestinationHashGenerator/DestinationHashGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.destination.DestinationHashGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationHashGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
